1fcb7dc3bc26a1a16a0183eb1b5a395e902a343c
Yago Cima Castelao <yago_castelau@hotmail.com>
2025-01-29 10:36:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,57 +10,48 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
 @app.route('/people', methods=['GET'])
-def get_all_people():
-    all_people = People.query.all()
-    return jsonify([{"id": people.id, "name": people.name} for people in all_people]), 200
+def list_people():
+    people = People.query.all()
+    return jsonify([{'id': person.id, 'name': person.name} for person in people])
+
+
 
 @app.route('/people/<int:people_id>', methods=['GET'])
 def get_person(people_id):
     person = People.query.get(people_id)
     if person:
-        return jsonify({
-            "id": person.id,
-            "name": person.name,
-            "height": person.height,
-            "mass": person.mass,
-            "hair_color": person.hair_color,
-            "skin_color": person.skin_color,
-            "eye_color": person.eye_color,
-            "birth_year": person.birth_year,
-            "gender": person.gender
-        }), 200
-    else:
-        return jsonify({"error": "Character not found"}), 404
+        return jsonify({'id': person.id, 'name': person.name})
+    return jsonify({'error': 'Person not found'}), 404
+
 
 
 @app.route('/planets', methods=['GET'])
-def get_all_planets():
-    all_planets = Planet.query.all()
-    
-    return jsonify([{"id": planet.id, "name": planet.name} for planet in all_planets]), 200
+def list_planets():
+    planets = Planet.query.all()
+    return jsonify([{'id': planet.id, 'name': planet.name} for planet in planets])
+
 
 
 @app.route('/planets/<int:planet_id>', methods=['GET'])
 def get_planet(planet_id):
     planet = Planet.query.get(planet_id)
     if planet:
-        return jsonify({
-            "id": planet.id,
-            "name": planet.name,
-            "climate": planet.climate,
-            "terrain": planet.terrain,
-            "population": planet.population
-        }), 200
-    else:
-        return jsonify({"error": "Planet not fond"}), 404
+        return jsonify({'id': planet.id, 'name': planet.name})
+    return jsonify({'error': 'Planet not found'}), 404
+
 
 
 @app.route('/users', methods=['GET'])
-def get_all_users():
-    all_users = User.query.all()
+def list_users():
+    users = User.query.all()
+    return jsonify([{'id': user.id, 'username': user.username} for user in users])
 
-    return jsonify([{"id": user.id, "username": user.username} for user in all_users]), 200
 
 
 @app.route('/users/favorites', methods=['GET'])
@@ -70,6 +61,7 @@ def list_favorites():
         favorites = Favorite.query.filter_by(user_id=user.id).all()
         return jsonify([{'id': favorite.id, 'planet_id': favorite.planet_id, 'people_id': favorite.people_id} for favorite in favorites])
     return jsonify({'error': 'User not found'}), 404
+
 
 
 @app.route('/favorite/planet/<int:planet_id>', methods=['POST'])
@@ -83,6 +75,7 @@ def add_favorite_planet(planet_id):
     return jsonify({'error': 'User not found'}), 404
 
 
+
 @app.route('/favorite/people/<int:people_id>', methods=['POST'])
 def add_favorite_people(people_id):
     user = User.query.get(1)
@@ -92,6 +85,7 @@ def add_favorite_people(people_id):
         db.session.commit()
         return jsonify({'message': 'People added to favorites'}), 201
     return jsonify({'error': 'User not found'}), 404
+
 
 
 @app.route('/favorite/planet/<int:planet_id>', methods=['DELETE'])
@@ -105,6 +99,7 @@ def delete_favorite_planet(planet_id):
             return jsonify({'message': 'Planet removed from favorites'}), 200
         return jsonify({'error': 'Favorite not found'}), 404
     return jsonify({'error': 'User not found'}), 404
+
 
 
 @app.route('/favorite/people/<int:people_id>', methods=['DELETE'])
